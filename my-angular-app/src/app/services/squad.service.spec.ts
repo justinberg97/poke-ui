be7d0d9b6e177e285bef195868c0e15a1e464bd5
Justin Berg <justin.berg1@verizon.com>
2025-02-25 14:28:28 -0600

    working on unit testing

--- my-angular-app/src/app/services/squad.service.spec.ts
+++ my-angular-app/src/app/services/squad.service.spec.ts
@@ -1,16 +1,37 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { SquadService } from './squad.service';
 
-// import { SquadService } from './squad.service';
+describe('SquadService', () => {
+  let service: SquadService;
 
-// describe('SquadService', () => {
-//   let service: SquadService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SquadService);
+  });
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(SquadService);
-//   });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should add a Pokémon to the squad', (done) => {
+    const pokemon = { name: 'Pikachu' };
+
+    service.addPokemonToSquad(pokemon);
+    service.squad$.subscribe(squad => {
+      expect(squad).toContain(pokemon);
+      done();
+    });
+  });
+
+  it('should remove a Pokémon from the squad', (done) => {
+    const pokemon = { name: 'Pikachu' };
+
+    service.addPokemonToSquad(pokemon);
+    service.removePokemonFromSquad(pokemon);
+
+    service.squad$.subscribe(squad => {
+      expect(squad).not.toContain(pokemon);
+      done();
+    });
+  });
+});
